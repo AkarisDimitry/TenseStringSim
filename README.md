c3466443703081d72fe02327df22039ca897b74e
Chunchunmaru <34775621+AkarisDimitry@users.noreply.github.com>
2023-08-10 12:31:46 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# **Simulación de Cuerda con Pygame**
+
+Este repositorio contiene un programa que simula el movimiento de una cuerda bajo diferentes fuerzas. Utiliza la librería Pygame para renderizar gráficamente la cuerda y permite al usuario interactuar con ella.
+
+## **Descripción**:
+
+La simulación representa una cuerda sometida a varias fuerzas:
+1. Elasticidad de la cuerda.
+2. Gravedad.
+3. Fricción.
+
+### **Física de la Simulación**:
+
+- **Elasticidad**:
+  La fuerza elástica en un segmento de la cuerda se basa en la Ley de Hooke:
+  \[ F_e = k \times (d - d_{eq}) \]
+  Donde:
+  - \( F_e \) es la fuerza elástica.
+  - \( k \) es la constante elástica de la cuerda.
+  - \( d \) es la longitud actual del segmento.
+  - \( d_{eq} \) es la longitud de equilibrio del segmento.
+
+- **Gravedad**:
+  La cuerda es afectada por la gravedad que actúa en la dirección vertical hacia abajo.
+  \[ F_g = m \times g \]
+  Donde:
+  - \( F_g \) es la fuerza gravitacional.
+  - \( m \) es la masa del segmento.
+  - \( g \) es la aceleración debido a la gravedad.
+
+- **Fricción**:
+  La fuerza de fricción actúa en dirección opuesta al movimiento y es proporcional a la velocidad del segmento:
+  \[ F_f = -\mu \times V \]
+  Donde:
+  - \( F_f \) es la fuerza de fricción.
+  - \( \mu \) es el coeficiente de fricción.
+  - \( V \) es la velocidad del segmento.
+
+### **Integración de Verlet**:
+
+Para simular el movimiento de la cuerda, se utiliza la técnica de integración de Verlet. Esta técnica es popular en simulaciones físicas por su estabilidad y precisión. El algoritmo Verlet básicamente usa las posiciones actuales y anteriores para estimar la siguiente posición.
+
+## **Funciones Principales**:
+
+- `integrate`: Esta función se encarga de calcular la posición y velocidad de cada punto en la cuerda en el siguiente paso de tiempo.
+  
+- `String`: Define la clase principal que representa la cuerda y contiene todos los parámetros y métodos necesarios para la simulación.
+
+## **Cómo Usar**:
+
+1. Asegúrate de tener instaladas las librerías `pygame`, `numpy` y `numba`.
+2. Ejecuta el código para iniciar la simulación.
+3. Interactúa con la cuerda utilizando el ratón.
+
+## **Contribución**:
+
+Si deseas contribuir al proyecto, no dudes en hacer un fork y enviar un pull request. Todas las contribuciones son bienvenidas.
+
---
+++ main.py
@@ -0,0 +1,124 @@
+import pygame
+import math
+import numpy as np
+from numba import jit, float64, int32, vectorize
+
+# Inicializar pygame
+pygame.init()
+
+# Definir colores
+c_BACK = (200, 200, 200)
+
+# Establecer dimensiones de la ventana
+ANCHO, ALTO = 800, 600
+Zx, Zy = 6, 40 
+X0, Y0 = 50, ALTO/2
+
+# Crear ventana y reloj
+ventana = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("Simulación de Cuerda")
+reloj = pygame.time.Clock()
+
+@jit('Tuple((float64[:,:], float64[:,:]))(int64, float64[:,:], float64[:,:], float64, float64, float64, float64, float64)', nopython=True)
+def integrate(N, X, V, dt, k, mu, m, deq):
+    for n in range(1000):
+        # === distances === 
+        dX = X[1:,:] - X[:-1,:]
+        d = (dX[:,0]**2 + dX[:,1]**2)**0.5
+        fdX = dX/d.reshape(-1, 1)
+
+        # === forces === 
+        Total_Force = np.zeros((N,2)) 
+
+        Fe = (d.reshape(-1, 1)-deq)*k*fdX 
+        Total_Force[:-1,:] += Fe
+        Total_Force[1:,:] -= Fe
+
+        Fg = np.ones(N)*m 
+        Total_Force[:,1] += Fg
+
+        Ff = V*mu 
+        Total_Force -= Ff
+
+        # === verlet 1st integration === 
+        V += Total_Force/10*dt/2
+        X[1:-1,:] += V[1:-1,:]*dt
+
+        # === distances === 
+        dX = X[1:,:] - X[:-1,:]
+        d = (dX[:,0]**2 + dX[:,1]**2)**0.5
+        fdX = dX/d.reshape(-1, 1)
+
+        # === forces === 
+        Total_Force = np.zeros((N,2)) 
+
+        Fe = (d.reshape(-1, 1)-deq)*k*fdX 
+        Total_Force[:-1,:] += Fe
+        Total_Force[1:,:] -= Fe
+
+        Fg = np.ones(N)*m 
+        Total_Force[:,1] += Fg
+
+        Ff = V*mu
+        Total_Force -= Ff
+
+        # === verlet 2nd integration === 
+        V += Total_Force/10*dt/2
+
+    return X, V
+
+# Definir clase para un segmento de la cuerda
+class String:
+    def __init__(self, N, X=None):
+        self.color = (150, 50, 50)
+        self.N = N
+        self.Ns = self.N-1
+        
+        self.dt = 0.09
+        self.dt2 = self.dt/2
+        
+        self.k = 1
+        self.d_eq = 1
+        self.m = 0.00001
+        self.mu = 0.001
+
+        self.X = X if not X is None else np.array( [np.arange(self.N, dtype=np.float64), np.sin(np.arange(self.N, dtype=np.float64)/10) * 0.5] , dtype=np.float64).T
+        
+        self.V = np.zeros_like(self.X, dtype=np.float64) 
+        self.a = np.zeros_like(self.X, dtype=np.float64) 
+
+        self.F = np.zeros_like(self.X, dtype=np.float64) 
+
+# Crear cuerda
+string = String(N=100)
+
+# Bucle principal
+RUN = True
+mover_cuerda = False
+while RUN:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            RUN = False
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            mover_cuerda = True
+        if evento.type == pygame.MOUSEBUTTONUP:
+            mover_cuerda = False
+
+    ventana.fill(c_BACK)
+
+    # Actualizar 
+    string.X, string.V = integrate(string.N, string.X, string.V, string.dt, string.k, string.mu, string.m, string.d_eq)
+
+    # dibujar segmentos de la cuerda
+    for Ni in range(1, string.N):
+        pygame.draw.line(ventana, string.color, ( string.X[Ni-1,0]*Zx+X0, string.X[Ni-1,1]*Zy+Y0 ), (string.X[Ni,0]*Zx+X0, string.X[Ni,1]*Zy+Y0 ), 2)
+
+    # Si el usuario está moviendo la cuerda
+    if mover_cuerda:
+        mx, my = pygame.mouse.get_pos()
+        string.X[1:-1, 1] -= np.e**( -(int((mx-X0)/Zx % string.N) - np.arange(string.N-2))**2/50 ) 
+
+    pygame.display.flip()
+    reloj.tick(100)
+
+pygame.quit()
